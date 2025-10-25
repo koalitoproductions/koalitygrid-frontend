14349d9d14b7d5720fda8d73be827d24d0c9439c
koalitoproductions <davidlindvall90@gmail.com>
2025-10-25 22:19:38 +0200

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.koalitygrid.se/api/',
+  baseURL: 'https://api.koalitygrid.se/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
